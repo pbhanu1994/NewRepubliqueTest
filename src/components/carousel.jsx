ec90b275ed9ba587b3490d8cdd92a058216e1849
Bhanu <pbhanu.1994@gmail.com>
2020-01-31 19:43:05 +1000

    Implemented the Carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import BrandStrategy from '../assets/images/HeroImage_BrandStrategy.jpg';
 import OurServices from '../assets/images/HeroImage_OurServices.jpg';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default class Carousel extends Component {
     render() {
       return (
-        <CarouselProvider
-          naturalSlideWidth={600}
-          naturalSlideHeight={480}
-          totalSlides={2}
-          orientation="vertical"
-        >
-        <Slider>
-          <Slide index={0}>
-            <img src={BrandStrategy} alt="Brand Strategy" />
-          </Slide>
-          <Slide index={1}>
-            <img src={OurServices} alt="Our Strategy" />
-          </Slide>
-        </Slider>
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
-        </CarouselProvider>
+        <React.Fragment>
+            <CarouselProvider
+                totalSlides={2}
+                orientation="vertical"
+                naturalSlideWidth={1280}
+                naturalSlideHeight={674}
+                isPlaying="true"
+                interval="3000"
+            >
+            <Slider>
+                <Slide index={0}>
+                    <img src={BrandStrategy} alt="Brand Strategy" />
+                </Slide>
+                <Slide index={1}>
+                    <img src={OurServices} alt="Our Strategy" />
+                </Slide>
+            </Slider>
+            <ButtonBack style={{display: "none"}}>Back</ButtonBack>
+            <ButtonNext style={{display: "none"}}>Next</ButtonNext>
+            </CarouselProvider>
+        </React.Fragment>
       );
     }
   }
