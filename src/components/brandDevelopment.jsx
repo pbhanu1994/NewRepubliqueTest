8265603c55a49c65323db94527ab39c667eeaeb2
Bhanu <pbhanu.1994@gmail.com>
2020-02-02 20:28:20 +1000

    Fixed the Section Items Padding at the bottom

--- src/components/brandDevelopment.jsx
+++ src/components/brandDevelopment.jsx
@@ -5,13 +5,13 @@ import '../styles/brandDevelopment.css';
 const BrandDevelopment = () => {
     return (
         <div className="BrandDevelopmentSection">
-            <div className="">
+            <div className="BrandDevelopmentItems">
                 <h1 className="BrandDevelopmentHeader">Brand Development</h1>
                 <p className="BrandDevelopmentParagraph">Our brand development process merges traditional brand planning methodologies with a new world digital approach to developing brand.</p>
                 <p className="BrandDevelopmentParagraph">Our process focuses on developing a brands position in market, tone or voice, personality and comrnunication platforrn.</p>
                 <a href="#" className="BrandDevelopmentLink"><span className="DigitalBusinessLinkText">view related work</span></a>
             </div>
-            <div className="">
+            <div>
                 <img className="BrandDevelopmentImage img-fluid" src={BrandDevelopmentImage} alt="Brand Development" />
             </div>
         </div>
--- src/components/digitalBusinessStrategy.jsx
+++ src/components/digitalBusinessStrategy.jsx
@@ -6,8 +6,10 @@ const DigitalBusiness = () => {
     return (
         <div className="row">
             <div className="DigitalBusinessSection col col-lg-12 col-md-12 col-sm-12" style={{paddingRight: "0"}}>
-                <img className="DigitalBusinessImage img-fluid" src={DigitalBusinessStrategy} alt="Digital Business Strategy" />
                 <div>
+                    <img className="DigitalBusinessImage img-fluid" src={DigitalBusinessStrategy} alt="Digital Business Strategy" />
+                </div>
+                <div className="DigitalBusinessItems">
                     <h1 className="DigitalBusinessHeader">Digital Business Strategy</h1>
                     <p className="DigitalBusinessParagraph">We work with many organisations to research and assess potential digital, mobile and new technology business models that can help them differentiate, create new revenues or reduce the cost of delivery.</p>
                     <p className="DigitalBusinessParagraph">Many organisations that approach us don't have the background, skill or know how to identify and asses how their organisation can take advantage of digital opportunities.</p>
