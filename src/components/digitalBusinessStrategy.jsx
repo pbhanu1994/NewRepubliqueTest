2ee9c21e963f1a3fa388f8d691894b58b1991d75
Bhanu <pbhanu.1994@gmail.com>
2020-02-02 14:16:56 +1000

    Added Responsiveness for Digital Business

--- src/components/digitalBusinessStrategy.jsx
+++ src/components/digitalBusinessStrategy.jsx
@@ -5,12 +5,16 @@ import '../styles/digitalBusiness.css';
 const DigitalBusiness = () => {
     return (
         <div className="DigitalBusinessSection">
-            <img className="DigitalBusinessImage" src={DigitalBusinessStrategy} alt="Digital Business Strategy" />
-            <div>
-                <h1 className="DigitalBusinessHeader">Digital Business Strategy</h1>
-                <p className="DigitalBusinessParagraph">We work with many organisations to research and assess potential digital, mobile and new technology business models that can help them differentiate, create new revenues or reduce the cost of delivery.</p>
-                <p className="DigitalBusinessParagraph">Many organisations that approach us don't have the background, skill or know how to identify and asses how their organisation can take advantage of digital opportunities.</p>
-                <a href="#" className="DigitalBusinessLink"><span className="DigitalBusinessLinkText">view related work</span></a>
+            <div className="row">
+                <div className="col col-lg-12 col-md-12 col-sm-12" style={{paddingRight: "0"}}>
+                    <img className="DigitalBusinessImage" src={DigitalBusinessStrategy} alt="Digital Business Strategy" />
+                    <div>
+                        <h1 className="DigitalBusinessHeader">Digital Business Strategy</h1>
+                        <p className="DigitalBusinessParagraph">We work with many organisations to research and assess potential digital, mobile and new technology business models that can help them differentiate, create new revenues or reduce the cost of delivery.</p>
+                        <p className="DigitalBusinessParagraph">Many organisations that approach us don't have the background, skill or know how to identify and asses how their organisation can take advantage of digital opportunities.</p>
+                        <a href="#" className="DigitalBusinessLink"><span className="DigitalBusinessLinkText">view related work</span></a>
+                    </div>
+                </div>
             </div>
         </div>
     );
